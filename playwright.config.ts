3d7a0567cd71e38cb33afd66b5824aa5cab8eed4
Taras <tkorpanets@gmail.com>
2025-08-07 16:43:12 +0300

    Change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
+/* Load environment variables from .env file */
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: './tests',
+  outputDir: 'test-results/',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,22 +16,44 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [['html'], ['allure-playwright']],
+  /* To use Allure reporter:
+  npm install -D allure-playwright
+  npm install -g allure-commandline
+  install Java JDK: https://adoptium.net/
+  npx playwright test --reporter=allure-playwright
+  allure generate ./allure-results -o ./allure-report
+  allure open ./allure-report
+  */
+  /* Timeout for each test */
+  timeout: 30 * 1000, // 30 seconds
+  /* Timeout for expect(...) */
+  expect: {
+    timeout: 5000,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://www.saucedemo.com/",
-
+    baseURL: 'https://www.saucedemo.com/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
+    //headless: true,
+    //viewport: { width: 1920, height: 1080 },
+    screenshot: 'only-on-failure',
+    /*IMPORTANT: To enable video recording, tests must be run via CLI (e.g., `npm run test`)
+    Video may not be saved when tests are run from the Playwright UI or VSCode extension */
+    // video: {
+    //   mode: 'retain-on-failure',
+    //   size: { width: 640, height: 480 },
+    // },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
+      name: 'chromium',
       use: {
-        ...devices["Desktop Chrome"],
+        ...devices['Desktop Chrome'],
         //viewport: { width: 1920, height: 1080 },
       },
     },
