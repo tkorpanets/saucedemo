b86033357c7c4695256b0ce2fb3d1345dcec99af
Taras <tkorpanets@gmail.com>
2025-08-27 18:02:15 +0300

    Ref: folders

---
+++ app/fixtures/index.ts
@@ -0,0 +1,28 @@
+import { test as base } from '@playwright/test';
+import { Application } from '..';
+import { users } from '../helpers/users';
+
+type AppFixture = { app: Application };
+
+export const test = base.extend<AppFixture>({
+  app: async ({ page }, use) => {
+    await use(new Application(page));
+  },
+});
+
+export const loginPageFixture = test.extend<AppFixture>({
+  app: async ({ app }, use) => {
+    await app.login.navigateToLoginPage();
+    await app.login.expectLoaded();
+    await use(app);
+  },
+});
+
+export const loggedUserFixture = loginPageFixture.extend<AppFixture>({
+  app: async ({ app }, use) => {
+    const { username, password } = users.standard;
+    await app.login.login(username, password);
+    await app.header.expectLoaded();
+    await use(app);
+  },
+});
---
+++ app/types/sorting.ts
@@ -0,0 +1,10 @@
+const SortValues = {
+  PriceLowHigh: 'Price (low to high)',
+  PriceHighLow: 'Price (high to low)',
+  NameAToZ: 'Name (A to Z)',
+  NameZToA: 'Name (Z to A)',
+} as const;
+
+export type PriceSort = typeof SortValues.PriceLowHigh | typeof SortValues.PriceHighLow;
+export type NameSort = typeof SortValues.NameAToZ | typeof SortValues.NameZToA;
+export type SortByValue = PriceSort | NameSort;
